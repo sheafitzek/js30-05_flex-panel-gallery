33e07b2448e8d4f66bc321a0e62323722042fbdb
Shea Fitzek <agg817@sbcglobal.net>
2017-08-31 23:22:19 -0500

    finished functionality

--- src/js/index.js
+++ src/js/index.js
@@ -13,8 +13,13 @@ const app = {
 		const sibs = app.siblings(this);
 
 		sibs.forEach((item)=> item.classList.remove(`open`, `active`));
-		this.classList.add(`open`);
-		setTimeout(()=> this.classList.add(`active`), 700);
+		if ([...this.classList].includes(`open`)) {
+			this.classList.remove(`open`);
+			setTimeout(()=> this.classList.remove(`active`), 700);
+		} else {
+			this.classList.add(`open`);
+			setTimeout(()=> this.classList.add(`active`), 700);
+		}
 	},
 
 	siblings(elem) {
